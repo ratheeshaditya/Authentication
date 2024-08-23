4fffd161b649240147edb4fad5581fd0139c2546
ratheeshaditya <ratheeshaditya@gmail.com>
2024-08-23 15:59:41 +0200

    Helper function to create the access token

---
+++ oauth_app/oauth/utils.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta, timezone
+import os
+import jwt
+from oauth.auth_api import SECRET_KEY,ALGORITHM,ACCESS_TOKEN_EXPIRE_MINUTES
+
+def create_access_token(data: dict):
+    """
+    Function to create the access token once the user is authenticated.
+    """
+    
+    to_encode = data.copy()
+    #Expiry is the current time + configured time. If no token expiry is provided, default is 15
+    expire = datetime.now(timezone.utc) + timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES) if ACCESS_TOKEN_EXPIRE_MINUTES else 15)
+    to_encode.update({"exp": expire}) #Add token expiry date
+    
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
